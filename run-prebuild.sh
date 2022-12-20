df03f87156945467684f0bde3656b7873eed6658
Ji Bin <matrixji@live.com>
2022-12-20 19:52:36 +0800

    master branch
    Signed-off-by: Ji Bin <matrixji@live.com>

--- run-prebuild.sh
+++ run-prebuild.sh
@@ -3,7 +3,7 @@
 ## variables
 
 MILVUS_REPO=${MILVUS_REPO:-https://github.com/matrixji/milvus.git}
-MILVUS_COMMIT=${MILVUS_COMMIT:-pms-2.2.1}
+MILVUS_COMMIT=${MILVUS_COMMIT:-master}
 BUILD_PROXY=
 
 export LANG=en_US.utf-8
