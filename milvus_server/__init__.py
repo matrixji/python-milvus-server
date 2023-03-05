21847e278f067ea1d0aaf4179b7895ac4cbd5d6b
Ji Bin <matrixji@live.com>
2023-03-05 16:28:20 +0800

    fix: support attributes dynamic

--- milvus_server/__init__.py
+++ milvus_server/__init__.py
@@ -13,6 +13,7 @@ import re
 import subprocess
 import socket
 from time import sleep
+from typing import Any, List
 
 __version__ = '2.2.3'
 
@@ -92,7 +93,10 @@ class MilvusServerConfig:
     def parse_template(self):
         """ parse template, lightweight template engine for avoid introducing dependencies like: yaml/Jinja2
 
-        We using {{ foo }} for variable and {{ bar: value }} for variable with default values
+        We using:
+        - {{ foo }} for variable
+        - {{ bar: value }} for variable with default values
+        - {{ bar(type) }} and {{ bar(type): value }} for type hint
         """
         for line in self.template_text.split('\n'):
             matches = re.match(r'.*\{\{(.*)}}.*', line)
@@ -100,26 +104,32 @@ class MilvusServerConfig:
                 text = matches.group(1)
                 original_key = '{{' + text + '}}'
                 text = text.strip()
+                value_type = str
                 if ':' in text:
                     key, val = text.split(':', maxsplit=2)
                     key, val = key.strip(), val.strip()
                 else:
                     key, val = text.strip(), None
+                if '(' in key:
+                    key, type_str = key.split('(')
+                    key, type_str = key.strip(), type_str.strip().replace(')', '')
+                    value_type = eval(type_str)
                 self.config_key_maps[original_key] = key
-                self.configurable_items[key] = val
+                self.configurable_items[key] = [
+                    value_type, self.get_value(val, value_type)]
         self.verbose_configurable_items()
 
     def verbose_configurable_items(self):
         for key, val in self.configurable_items.items():
             self.logger.debug(
-                'Configurable item %s with default: %s', key, val)
+                'Configurable item %s(%s) with default: %s', key, val[0], val[1])
 
     def resolve(self):
         self.cleanup_listen_ports()
         self.resolve_all_listen_ports()
         self.resolve_storage()
         for key, value in self.configurable_items.items():
-            if value is None:
+            if value[1] is None:
                 raise RuntimeError(
                     f'{key} is still not resolved, please try specify one.')
         # ready to start
@@ -142,7 +152,7 @@ class MilvusServerConfig:
                         'bind port %d success, using it as %s', port, port_key)
                 self.listen_ports[port_key] = (port, sock)
             else:
-                port_start = self.configurable_items[port_key] or self.RANDOM_PORT_START
+                port_start = self.configurable_items[port_key][1] or self.RANDOM_PORT_START
                 port_start = int(port_start)
                 for i in range(10000):
                     port = port_start + i
@@ -153,7 +163,7 @@ class MilvusServerConfig:
                             'bind port %d success, using it as %s', port, port_key)
                         break
         for port_key, data in self.listen_ports.items():
-            self.configurable_items[port_key] = data[0]
+            self.configurable_items[port_key][1] = data[0]
 
     @classmethod
     def try_bind_port(cls, port):
@@ -176,6 +186,24 @@ class MilvusServerConfig:
         default_dir = expandvars('${HOME}')
         return join(default_dir, '.milvus.io', 'milvus-server')
 
+    @classmethod
+    def get_value_text(cls, val) -> str:
+        if isinstance(val, bool):
+            return 'true' if val else 'false'
+        if isinstance(val, str):
+            return f'"{val}"'
+        return str(val)
+
+    @classmethod
+    def get_value(cls, text, val_type) -> Any:
+        if val_type == bool:
+            return True if text == 'true' else False
+        if val_type == int:
+            if not text:
+                return 0
+            return int(text)
+        return text
+
     def resolve_storage(self):
         self.base_data_dir = self.configs.get(
             'data_dir', self.get_default_data_dir())
@@ -189,23 +217,26 @@ class MilvusServerConfig:
 
         # logs
         if sys.platform.lower() == 'win32':
-            self.configurable_items['etcd_log_path'] = 'winfile:///' + \
-                join(logs_dir, 'etcd.log').replace('\\', '/')
+            self.set('etcd_log_path', 'winfile:///' +
+                     join(logs_dir, 'etcd.log').replace('\\', '/'))
         else:
-            self.configurable_items['etcd_log_path'] = join(
-                logs_dir, 'etcd.log')
-        self.configurable_items['proxy_log_dir'] = logs_dir
-        self.configurable_items['proxy_log_name'] = 'proxy.log'
-        self.configurable_items['system_log_path'] = join(
-            logs_dir, 'system.log')
+            self.set('etcd_log_path', join(logs_dir, 'etcd.log'))
+        self.set('system_log_path', logs_dir)
 
         # data
-        self.configurable_items['etcd_data_dir'] = join(
-            storage_dir, 'etcd.data')
-        self.configurable_items['local_storage_dir'] = join(
-            storage_dir, 'storage')
-        self.configurable_items['rocketmq_data_dir'] = join(
-            storage_dir, 'rocketmq')
+        self.set('etcd_data_dir', join(storage_dir, 'etcd.data'))
+        self.set('local_storage_dir', join(storage_dir, 'storage'))
+        self.set('rocketmq_data_dir', join(storage_dir, 'rocketmq'))
+
+    def get(self, attr) -> Any:
+        return self.configurable_items[attr][1]
+
+    def get_type(self, attr) -> Any:
+        return self.configurable_items[attr][0]
+
+    def set(self, attr, val) -> None:
+        if type(val) == self.configurable_items[attr][0]:
+            self.configurable_items[attr][1] = val
 
     def cleanup_listen_ports(self):
         for data in self.listen_ports.values():
@@ -217,18 +248,14 @@ class MilvusServerConfig:
         config_file = join(self.base_data_dir, 'configs', 'milvus.yaml')
         content = self.template_text
         for key, val in self.config_key_maps.items():
-            value = self.configurable_items[val]
-            if isinstance(value, str):
-                value_text = "'" + value + "'"
-            else:
-                value_text = str(value)
+            value = self.configurable_items[val][1]
+            value_text = self.get_value_text(value)
             content = content.replace(key, value_text)
         with open(config_file, 'w', encoding='utf-8') as config:
             config.write(content)
 
 
 class MilvusServer:
-
     def __init__(self, config: MilvusServerConfig = None, **kwargs):
         """_summary_
 
@@ -327,20 +354,16 @@ class MilvusServer:
         return '127.0.0.1'
 
     @property
+    def config_keys(self) -> List[str]:
+        return self.config.configurable_items.keys()
+
+    @property
     def listen_port(self) -> int:
-        return int(self.config.configurable_items.get('proxy_port', 0))
+        return int(self.config_get('proxy_port', 0))
 
     @listen_port.setter
     def listen_port(self, val: int):
-        self.config.configurable_items['proxy_port'] = val
-
-    @property
-    def authorization_enabled(self) -> bool:
-        return self.config.configurable_items.get('authorization_enabled', 'false') == 'true'
-
-    @authorization_enabled.setter
-    def authorization_enabled(self, val: bool):
-        self.config.configurable_items['authorization_enabled'] = 'true' if val else 'false'
+        self.config_set('proxy_port', val)
 
     @property
     def debug(self):
@@ -352,16 +375,6 @@ class MilvusServer:
         self.logger = _create_logger('debug' if val else 'null')
         self.config.logger = self.logger
 
-    def apply_config(self, key: str, val: str):
-        if hasattr(self, key):
-            val_type = type(getattr(self, key))
-            if val_type == bool:
-                val = val.lower() in ('true', 'yes')
-            else:
-                val = type(getattr(self, key))(val)
-            setattr(self, key, val)
-            self.logger.info('set %s=%s', key, val)
-
 
 _initialize_data_files()
 default_server = MilvusServer()
@@ -370,10 +383,20 @@ debug_server = MilvusServer(MilvusServerConfig(), debug=True)
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--set', action='append', dest='values')
     parser.add_argument('--debug', action='store_true',
-                        dest='debug', default=False)
-    parser.add_argument('--data', dest='data_dir', default='')
+                        dest='debug', default=False, help='enable debug')
+    parser.add_argument('--data', dest='data_dir', default='',
+                        help='set base data dir for milvus')
+
+    # dynamic configurations
+    for key in default_server.config_keys:
+        val = default_server.config.get(key)
+        if val is not None:
+            val_type = default_server.config.get_type(key)
+            name = '--' + key.replace('_', '-')
+            parser.add_argument(name, type=val_type, default=val,
+                                dest=f'x_{key}', help=f'set value for {key} ({val_type.__name__})')
+
     args = parser.parse_args()
 
     # select server
@@ -384,13 +407,15 @@ def main():
         server.set_base_dir(args.data_dir)
 
     # apply configs
-    for expression in args.values or []:
-        if '=' in expression:
-            key, val = expression.split('=', 1)
-            key, val = key.strip(), val.strip()
-            server.apply_config(key, val)
+    for name, value in args._get_kwargs():
+        if name.startswith('x_'):
+            server.config.set(name[2:], value)
 
     signal.signal(signal.SIGINT, lambda sig, h: server.stop())
 
     server.start()
     server.wait()
+
+
+if __name__ == '__main__':
+    main()
